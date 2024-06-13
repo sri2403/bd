866e064e6bde06927b06b6f73c129da245daca37
sri2403 <sriharinimuthukrishnan@gmail.com>
2024-06-13 11:49:37 +0530

    first

--- index.js
+++ index.js
@@ -17,12 +17,14 @@ app.use("/api",router);
 connectDB();
 app.get("/",(req,res)=>{
     res.status(200).send(`
-    <h1>Node Day 5 Task</h1>
-    <p><strong>/api/register:</strong> Create a new user account.</p>
-    <p><strong>/api/login:</strong> Log in to an existing user account.</p>
-    <p><strong>/api/forgot-password:</strong> Reset your password if forgotten.</p>
-    <p>This is the main endpoint of our Node.js application for day 5 tasks, 
-    providing information about user account-related functionalities.</p>
+    <h1>Node Day 6 Task</h1>
+<p><strong>/register:</strong> Create a new user account.</p>
+<p><strong>/login:</strong> Log in to an existing user account.</p>
+<p><strong>/forgot-password:</strong> Reset your password if forgotten.</p>
+<p><strong>/get-user:</strong> Retrieve user details if authenticated.</p>
+<p><strong>/shortUrls:</strong> Shorten a given URL.</p>
+<p>This is the main endpoint of our Node.js application for day 5 tasks, providing information about user account-related functionalities and URL shortening.</p>
+
 `)
 })
 app.listen(process.env.PORT||3000,()=>{
