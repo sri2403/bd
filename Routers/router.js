c797150899e9e4a2781dd6ebf0c4d158394bb769
sri2403 <sriharinimuthukrishnan@gmail.com>
2024-06-25 19:52:11 +0530

    one

--- Routers/router.js
+++ Routers/router.js
@@ -1,5 +1,5 @@
 import  express  from "express";
-import { forgotPassword, getuser, loginUser, registerUser, urlShort } from "../Controllers/controller.js";
+import { forgotPassword,  loginUser, registerUser, urlShort } from "../Controllers/controller.js";
 import authMiddleware from "../Middleware/middleware.js";
 
 const router=express.Router();
@@ -7,7 +7,6 @@ const router=express.Router();
 router.post("/register",registerUser)
 router.post("/login",loginUser)
 router.post("/forgot-password",forgotPassword)
-// router.get("/get-user",authMiddleware,getuser)
 router.post("/shortUrls",authMiddleware,urlShort)
 
 export default router;
